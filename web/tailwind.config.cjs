af40fe47fff0fbddef63e9433dbc9b13ca000e4e
Nathan Seixeiro <nathan.seixeiro@gmail.com>
2022-09-13 16:02:28 -0300

    finish layout frontend web

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx',],
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'duo-gradient': 'linear-gradient(89.86deg, #9572FC 15.08%, #43E7AD 35.94%, #E1D55D 80.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      },
+    },
   },
   plugins: [],
 }
