f719004e11d719b20bd23f4f2ad4786c530bcf96
Nathan Seixeiro <nathan.seixeiro@gmail.com>
2022-09-15 13:57:55 -0300

    database creation with prisma orm and primary test api of games

---
+++ server/README.md
@@ -0,0 +1,29 @@
+## BACKEND
+
+### Entities 
+
+#### Game 
+
+    id
+    title
+    bannerurl
+#### ad
+
+    id 
+    gameId
+    name
+    yearsPlaying
+    discord
+    weekDays
+    hoursStart
+    hoursEnd 
+    useVoiceChannel 
+    createdAt
+
+### Casos de uso 
+
+    listagem de games com contagem de ads
+    criaçao de novos anuncios
+    listagem de ads por game 
+    buscar discord pelo id do anuncio 
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,34 @@
-import express from "express"
+import express, {  } from "express";
+import {PrismaClient} from '@prisma/client'
 
-const app = express()
+const app = express();
+const prisma = new PrismaClient()
 
-app.get('/ads', (request, response) => {
-    return response.json([
-        { id: 1, name: 'Anuncio 1' },
-        { id: 2, name: 'Anuncio 2' },
-        { id: 3, name: 'Anuncio 3' },
-        { id: 4, name: 'Anuncio 4' }
-    ])
+app.get("/games", async (request, response) => {
+    const games = await prisma.game.findMany({})
+  return response.json(games);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(200).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  //   const gameId = request.params.id;
+  return response.json([
+    { id: 1, name: "Anuncio 1" },
+    { id: 2, name: "Anuncio 2" },
+    { id: 3, name: "Anuncio 3" },
+    { id: 4, name: "Anuncio 4" },
+    { id: 5, name: "Anuncio 5" },
+  ]);
+});
+
+app.get('/ads/:id/discord', (request, response) =>{
+    // const adsId = req.params.id;
+
+    return response.json([]);
 })
-app.listen(3333)
+
+
+app.listen(3333);
