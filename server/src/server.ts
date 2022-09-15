c9a538bfdf581126b4f266b6f9008e8ec034ec0a
Nathan Seixeiro <nathan.seixeiro@gmail.com>
2022-09-15 14:37:40 -0300

    creation api show ads and tests

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,34 +1,60 @@
-import express, {  } from "express";
-import {PrismaClient} from '@prisma/client'
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 app.get("/games", async (request, response) => {
-    const games = await prisma.game.findMany({})
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
   return response.json(games);
-});
+}); // list games and quantity ads
 
 app.post("/ads", (request, response) => {
   return response.status(200).json([]);
-});
+}); //created ad
 
-app.get("/games/:id/ads", (request, response) => {
-  //   const gameId = request.params.id;
-  return response.json([
-    { id: 1, name: "Anuncio 1" },
-    { id: 2, name: "Anuncio 2" },
-    { id: 3, name: "Anuncio 3" },
-    { id: 4, name: "Anuncio 4" },
-    { id: 5, name: "Anuncio 5" },
-  ]);
-});
+app.get("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
+  const ads = await prisma.ad.findMany({
+    select: {
+        id: true,
+        name: true,
+        weekDays: true,
+        useVoiceChannel: true,
+        hoursStart: true,
+        hoursEnd: true,
+        yearsPlaying: true,
+    },
+    where: {
+        gameId,
+    },
+    orderBy:{
+        createdAt: 'desc',
+    }
 
-app.get('/ads/:id/discord', (request, response) =>{
-    // const adsId = req.params.id;
+  })
+  return response.json(ads.map(ad => {
+    return {
+        ...ad,
+        weekDays: ad.weekDays.split(','),
+       // hourStart: convertMinutesToHourString(ad.hourStart),
+       // hourEnd: convertMinutesToHourString(ad.hourEnd),
+      }
+    }));
+  }); // list ads by game
 
-    return response.json([]);
-})
+app.get("/ads/:id/discord", (request, response) => {
+  // const adsId = req.params.id;
 
+  return response.json([]);
+}); //show discord
 
 app.listen(3333);
