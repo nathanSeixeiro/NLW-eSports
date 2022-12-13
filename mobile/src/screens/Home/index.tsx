54db81920de06b0a77747c0fc96510f22e3febe9
Nathan Seixeiro <nathan.seixeiro@gmail.com>
2022-12-13 12:24:46 -0300

    finalização do anuncio

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,21 +1,30 @@
-import { View, Image, FlatList } from "react-native";
+import { useEffect, useState } from "react";
+import { styles } from "./styles";
+import * as Heading from "../../components/Heading";
 import logo from "../../assets/logo-nlw-esports.png";
 import { GameCard } from "../../components/GameCard";
-import { Heading } from "../../components/Heading";
-import { styles } from "./styles";
-import { GAMES } from "./../../utils/games";
+import { View, Image, FlatList } from "react-native";
+import { GameCardProps } from './../../components/GameCard/index';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  useEffect(() => {
+    fetch("http://192.168.0.2:3333/games")
+      .then((response) => response.json())
+      .then((data) => setGames(data));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
-      <Heading
+      <Heading.Heading
         title="Find's your duo!"
         subtitle="Select the game you want to play"
       />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         showsHorizontalScrollIndicator={false}
